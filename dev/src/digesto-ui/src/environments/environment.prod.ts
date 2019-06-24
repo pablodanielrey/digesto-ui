2321cfa43c534b07d3e0b9a72b11d59d391b06b3
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-06-24 05:05:54 -0300

    agregue la parte de producción y corregi algunos errores que impedían la compilación.

--- dev/src/digesto-ui/src/environments/environment.prod.ts
+++ dev/src/digesto-ui/src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
-  production: true
+  production: true,
+  loginApiUrl: 'https://api.econo.unlp.edu.ar/login/api/v1.0',
+  wardenApiUrl: 'https://api.econo.unlp.edu.ar/warden/api/v1.0',
+  oidp_issuer: 'https://oidc.econo.unlp.edu.ar/',
+  digestoApiUrl: 'https://api.econo.unlp.edu.ar/digesto/api/v1.0',
+
+  client_id: 'digesto-ui',
+  version: '0.0.1a',
+
+  loader: {
+    cabecera: 'DIGESTO | FCE',
+    version: '0.0.1a',
+    tituloSistema: 'DIGESTO',
+    subtituloSistema: 'Normativas de la Facultad de Ciencias Económicas',
+    desarrolloSistema: 'DiTeSI | Dirección de Tecnologías y Sistemas Informáticos',
+    logoSistema: '/assets/img/fce/logofce2018.png',
+  }       
 };
---
+++ prod/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo docker build -t digesto-ui-prod -f docker/dockerfile .
