5d661352b5ad289b08ac5acbe46e570f43f24425
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-07-02 11:56:43 -0300

    paso a producción la nueva version

--- dev/src/digesto-ui/src/environments/environment.prod.ts
+++ dev/src/digesto-ui/src/environments/environment.prod.ts
@@ -6,11 +6,11 @@ export const environment = {
   digestoApiUrl: 'https://api.econo.unlp.edu.ar/digesto/api/v1.0',
 
   client_id: 'digesto-ui',
-  version: '0.1.0',
+  version: '0.1.1',
 
   loader: {
     cabecera: 'DIGESTO | FCE',
-    version: '0.1.0',
+    version: '0.1.1',
     tituloSistema: 'DIGESTO',
     subtituloSistema: 'Normativas de la Facultad de Ciencias Económicas',
     desarrolloSistema: 'DiTeSI | Dirección de Tecnologías y Sistemas Informáticos',
