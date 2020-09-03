5925c1c0d226000db71896b63401aea31fa6b4a9
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-09-03 19:53:29 -0300

    cambie la url de las apis

--- dev/src/digesto-ui/src/environments/environment.prod.ts
+++ dev/src/digesto-ui/src/environments/environment.prod.ts
@@ -1,16 +1,16 @@
 export const environment = {
   production: true,
-  loginApiUrl: 'https://api.econo.unlp.edu.ar/login/api/v1.0',
-  wardenApiUrl: 'https://api.econo.unlp.edu.ar/warden/api/v1.0',
+  loginApiUrl: 'https://api.login.econo.unlp.edu.ar/login/api/v1.0',
+  wardenApiUrl: 'https://api.warden.econo.unlp.edu.ar/warden/api/v1.0',
   oidp_issuer: 'https://oidc.econo.unlp.edu.ar/',
-  digestoApiUrl: 'https://api.econo.unlp.edu.ar/digesto/api/v1.0',
+  digestoApiUrl: 'https://api.digesto.econo.unlp.edu.ar/digesto/api/v1.0',
 
   client_id: 'digesto-ui',
-  version: '0.1.2',
+  version: '0.1.3',
 
   loader: {
     cabecera: 'DIGESTO | FCE',
-    version: '0.1.2',
+    version: '0.1.3',
     tituloSistema: 'DIGESTO',
     subtituloSistema: 'Normativas de la Facultad de Ciencias Económicas',
     desarrolloSistema: 'DiTeSI | Dirección de Tecnologías y Sistemas Informáticos',
